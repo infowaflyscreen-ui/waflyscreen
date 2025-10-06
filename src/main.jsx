8e6a429b7792193a833630ef29d18655ae12141d
waflyscreen <info.waflyscreen@gmail.com>
2025-10-06 09:58:27 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,33 +1,16 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.jsx'
-// import { BrowserRouter, HashRouter  } from 'react-router-dom'
-// import { AuthProvider } from './contexts/AuthContext'
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//         <HashRouter>
-//           <AuthProvider>
-//             <App />
-//           </AuthProvider>
-//         </HashRouter>
-//    </StrictMode>,
-// )
-
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter  } from 'react-router-dom'
 import { AuthProvider } from './contexts/AuthContext'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
-  </StrictMode>
+        <HashRouter>
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+        </HashRouter>
+   </StrictMode>,
 )
